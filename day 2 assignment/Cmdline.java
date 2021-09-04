ec913789da5c38171380cc83368c48771a8f24cb
Vishnutpgit <89830986+Vishnutpgit@users.noreply.github.com>
2021-09-04 18:56:53 +0530

    Day 2 Assignment

---
+++ day 2 assignment/Cmdline.java
@@ -0,0 +1,8 @@
+public class Cmdline{
+ public static void main(String[]args){
+int x = Integer.parseInt(args[0]);
+int y = Integer.parseInt(args[1]);
+int z =  x + y;
+System.out.println("sum of two numbers is "+ z);
+}
+}
